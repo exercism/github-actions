118d84846b7b8ccb8b3b995d206277527c5572ac
Sascha Mann <git@mail.saschamann.eu>
2020-12-16 15:05:20 +0100

    Squashed 'list-pr-files/' content from commit d664265
    git-subtree-dir: list-pr-files
    git-subtree-split: d6642653547c64226db6681b5222a33e5c986fcd

---
+++ README.md
@@ -0,0 +1,1 @@
+# list-pr-files
---
+++ src/index.js
@@ -0,0 +1,27 @@
+const core = require("@actions/core");
+const github = require("@actions/github");
+
+async function main() {
+  try {
+    const token = core.getInput("ghToken");
+    const githubClient = new github.GitHub(token);
+
+    const contextPayload = github.context.payload;
+
+    const endpointOptions = githubClient.pulls.listFiles.endpoint.merge({
+      owner: contextPayload.repository.owner.login,
+      repo: contextPayload.repository.name,
+      pull_number: contextPayload.pull_request.number,
+    });
+
+    const files = await githubClient.paginate(endpointOptions, (response) =>
+      response.data.map((file) => file.filename)
+    );
+
+    core.setOutput("files", JSON.stringify(files));
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+}
+
+main();
